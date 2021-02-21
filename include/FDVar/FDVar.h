7967c0b3209000f22a929ac5298744b7d141593b
Benjamin Zamour <benjamin.zamour@gmail.com>
2021-02-21 11:06:04 +0100

    Update to C++20 + Move FDVarConfig + Update CMakeLists.txt

---
+++ include/FDVar/FDVar.h
@@ -0,0 +1,11 @@
+#ifndef FDVAR_FDVAR_H
+#define FDVAR_FDVAR_H
+
+#include <FDVar/DynamicVariable.h>
+
+namespace FDVar
+{
+    typedef DynamicVariable var;
+}
+
+#endif // FDVAR_FDVAR_H
---
+++ include/FDVar/FDVarConfig.h
@@ -0,0 +1,9 @@
+#ifndef FDVAR_FDVARCONFIG_H
+#define FDVAR_FDVARCONFIG_H
+
+// clang-format off
+constexpr int FDVar_VERSION_MAJOR = 0;
+constexpr int FDVar_VERSION_MINOR = 1;
+// clang-format on
+
+#endif // FDVAR_FDVARCONFIG_H
